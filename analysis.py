a08196c3768817561ba22e5f197ad10ea9c2e6f3
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-09-27 13:16:29 -0400

    pushed prograM'

--- analysis.py
+++ analysis.py
@@ -1,2 +1,4 @@
 # https://data.ny.gov/resource/wujg-7c2s.json -> API
 
+# make a simple API call to this -> API: https://data.ny.gov/resource/wujg-7c2s.json?transit_timestamp=2022-02-01T00:00:00.000 
+
